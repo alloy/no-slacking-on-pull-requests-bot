3f4bee0535e445ad005a3f0ad18c4dd2fe55aebd
Brad Harris <bmharris@gmail.com>
2016-05-17 16:38:33 -0600

    adding support for multi-team (#3)

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var Botkit = require('botkit')
 
-// Expect a SLACK_TOKEN environment variable
-var slackToken = process.env.SLACK_TOKEN
-if (!slackToken) {
-  console.error('SLACK_TOKEN is required!')
-  process.exit(1)
+var token = process.env.SLACK_TOKEN
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
 }
-
-var controller = Botkit.slackbot()
-var bot = controller.spawn({
-  token: slackToken
-})
-
-bot.startRTM(function (err, bot, payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack')
-  }
-})
 
 controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
