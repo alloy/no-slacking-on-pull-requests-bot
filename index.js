9cbc72ef4b0f0e1c1548f452e9f43a4cd4f3bfa3
Eloy Durán <eloy.de.enige@gmail.com>
2016-12-03 12:13:08 -0500

    Make ESLint and Flow green.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Botkit = require('botkit')
-var request = require('superagent');
+import Botkit from "botkit"
+// var request = require("superagent")
 
 var token = process.env.SLACK_TOKEN
 
@@ -9,107 +9,112 @@ var controller = Botkit.slackbot({
   debug: false
 })
 
-var Bot = null;
+var Bot = null
 
 // Assume single team mode if we have a SLACK_TOKEN
 if (token) {
-  console.log('Starting in single-team mode')
+  console.log("Starting in single-team mode")
   controller.spawn({
     token: token,
     retry: Infinity
-  }).startRTM(function (err, bot, payload) {
+  }).startRTM((err, bot, payload) => {
     if (err) {
       throw new Error(err)
     }
-    Bot = bot;
-    console.log('Connected to Slack RTM')
+    Bot = bot
+    console.log("Connected to Slack RTM")
   })
 // Otherwise assume multi-team mode - setup beep boop resourcer connection
 } else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
+  console.log("Starting in Beep Boop multi-team mode")
+  require("beepboop-botkit").start(controller, { debug: true })
 }
 
-controller.on('bot_channel_join', function (bot, message) {
+controller.on("bot_channel_join", (bot, message) => {
   bot.reply(message, "I'm here!")
 })
 
-controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
-  bot.reply(message, 'Hello.')
+controller.hears(["hello", "hi"], ["direct_mention"], (bot, message) => {
+  bot.reply(message, "Hello.")
 })
 
-controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
-  bot.reply(message, 'Hello.')
-  bot.reply(message, 'It\'s nice to talk to you directly.')
+controller.hears(["hello", "hi"], ["direct_message"], (bot, message) => {
+  bot.reply(message, "Hello.")
+  bot.reply(message, "It's nice to talk to you directly.")
 })
 
-controller.hears('.*', ['mention'], function (bot, message) {
-  bot.reply(message, 'You really do care about me. :heart:')
+controller.hears(".*", ["mention"], (bot, message) => {
+  bot.reply(message, "You really do care about me. :heart:")
 })
 
-controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
-  var help = 'I will respond to the following messages: \n' +
-      '`bot hi` for a simple message.\n' +
-      '`bot attachment` to see a Slack attachment message.\n' +
-      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
-      '`bot help` to see this again.'
+controller.hears("help", ["direct_message", "direct_mention"], (bot, message) => {
+  var help = "I will respond to the following messages: \n" +
+      "`bot hi` for a simple message.\n" +
+      "`bot attachment` to see a Slack attachment message.\n" +
+      "`@<your bot's name>` to demonstrate detecting a mention.\n" +
+      "`bot help` to see this again."
   bot.reply(message, help)
 })
 
-controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
-  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+controller.hears(["attachment"], ["direct_message", "direct_mention"], (bot, message) => {
+  var text = "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
   var attachments = [{
     fallback: text,
-    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
-    title: 'Host, deploy and share your bot in seconds.',
-    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
-    title_link: 'https://beepboophq.com/',
+    pretext: "We bring bots to life. :sunglasses: :thumbsup:",
+    title: "Host, deploy and share your bot in seconds.",
+    image_url: "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
+    title_link: "https://beepboophq.com/",
     text: text,
-    color: '#7CD197'
+    color: "#7CD197"
   }]
 
   bot.reply(message, {
     attachments: attachments
-  }, function (err, resp) {
+  }, (err, resp) => {
     console.log(err, resp)
   })
 })
 
 /* --------- */
 
-var Users = {};
-var OpenPullRequests = {};
+var Users = {}
+var OpenPullRequests = {}
 
-function mapPullRequests(pullRequests) {
-  return pullRequests.map(function (pullRequest) {
-    return {
-      url: pullRequest.html_url,
-      createdAt: pullRequest.created_at,
-      title: pullRequest.title,
-      repo: pullRequest.repository.name,
-      number: pullRequest.number,
-    };
-  });
-}
+// function mapPullRequests(pullRequests: any[]) {
+//   return pullRequests.map((pullRequest) => {
+//     return {
+//       url: pullRequest.html_url,
+//       createdAt: pullRequest.created_at,
+//       title: pullRequest.title,
+//       repo: pullRequest.repository.name,
+//       number: pullRequest.number
+//     }
+//   })
+// }
 
-function fetchPullRequests(token, callback, pullRequests = [], page = 1) {
-  console.log('Request page ' + page);
-  request
-    .get('https://api.github.com/orgs/artsy/issues')
-    .query({ state: 'open', filter: 'assigned', page: page })
-    .set('Authorization', 'token ' + token)
-    .accept('json')
-    .catch(function(error) { console.log('ERROR: ' + error); })
-    .then(function(response) {
-      pullRequests = pullRequests.concat(response.body.filter(function (issue) { return issue.pull_request; }));
-      var link = response.header['link'];
-      if (link && link.includes('rel="last"')) {
-        fetchPullRequests(token, callback, pullRequests, page + 1)
-      } else {
-        callback(pullRequests);
-      }
-    });
-}
+// function fetchPullRequests(
+//     token: string,
+//     callback: (pullRequests: any[]) => void,
+//     pullRequests: any[] = [],
+//     page: number = 1
+//   ) {
+//   console.log("Request page " + page)
+//   request
+//     .get("https://api.github.com/orgs/artsy/issues")
+//     .query({ state: "open", filter: "assigned", page: page })
+//     .set("Authorization", "token " + token)
+//     .accept("json")
+//     .catch((error) => console.log("ERROR: " + error))
+//     .then((response) => {
+//       pullRequests = pullRequests.concat(response.body.filter((issue) => issue.pull_request))
+//       var link = response.header["link"]
+//       if (link && link.includes('rel="last"')) {
+//         fetchPullRequests(token, callback, pullRequests, page + 1)
+//       } else {
+//         callback(pullRequests)
+//       }
+//     })
+// }
 
 // function fetchPullRequestEvents(token, username) {
 //   request
@@ -125,13 +130,13 @@ function fetchPullRequests(token, callback, pullRequests = [], page = 1) {
 //     });
 // }
 
-controller.hears('register', ['direct_message'], function(bot, message) {
-  console.log(message);
-  var matches = message.text.match(/^register ([a-z0-9-]{0,38})\s*([a-z0-9]+)?/i);
+controller.hears("register", ["direct_message"], (bot, message) => {
+  console.log(message)
+  var matches = message.text.match(/^register ([a-z0-9-]{0,38})\s*([a-z0-9]+)?/i)
   if (matches) {
-    var username = matches[1];
-    Users[username] = message.user;
-    console.log(Users);
+    var username = matches[1]
+    Users[username] = message.user
+    console.log(Users)
     // var token = matches[2];
     // fetchPullRequests(token, function (pullRequests) {
     //   var attachments = mapPullRequests(pullRequests).map(function (pullRequest) {
@@ -143,47 +148,49 @@ controller.hears('register', ['direct_message'], function(bot, message) {
     //   bot.reply(message, { attachments: attachments });
     // });
   } else {
-    bot.reply(message, 'Usage: `register github-handle access-token`');
+    bot.reply(message, "Usage: `register github-handle access-token`")
   }
-});
+})
 
+controller.hears(".*", ["direct_message", "direct_mention"], (bot, message) => {
+  bot.reply(message, "Sorry <@" + message.user + ">, I don't understand. \n")
+})
 
-
-
-controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
-  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
-});
-
-
-
-
-controller.setupWebserver(process.env.PORT || 3000, function(err, server) {
-  server.post('/', function (req, res) {
+controller.setupWebserver(process.env.PORT || 3000, (err, server) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  server.post("/", (req, res) => {
     // Even though the webhook should be configured to only send PR events, GitHub will send an initial setup event.
-    if (req.headers['x-github-event'] === 'pull_request') {
-      var data = req.body;
-      console.log(data);
+    if (req.headers["x-github-event"] === "pull_request") {
+      var data = req.body
+      console.log(data)
       // Newly opened PRs that are assigned will also have action `assigned`.
-      if (data.action === 'assigned' || data.action === 'unassigned') {
-        var assignees = data.pull_request.assignees.map(function (assignee) { return assignee.login; });
-        var pullRequestID = data.pull_request.id;
-        var previousPullRequest = OpenPullRequests[pullRequestID];
-        var previousAssignees = (previousPullRequest && previousPullRequest.assignees) || [];
+      if (data.action === "assigned" || data.action === "unassigned") {
+        var assignees = data.pull_request.assignees.map((assignee) => assignee.login)
+        var pullRequestID = data.pull_request.id
+        var previousPullRequest = OpenPullRequests[pullRequestID]
+        var previousAssignees = (previousPullRequest && previousPullRequest.assignees) || []
         if (previousAssignees !== assignees) {
-          OpenPullRequests[data.pull_request.id] = { id: pullRequestID, assignees: assignees };
-          var assigned = assignees.filter(function (assignee) { return !previousAssignees.includes(assignee); });
-          assigned.forEach(function (assignee) {
-            var user = Users[assignee];
+          OpenPullRequests[data.pull_request.id] = { id: pullRequestID, assignees: assignees }
+          var assigned = assignees.filter((assignee) => !previousAssignees.includes(assignee))
+          assigned.forEach((assignee) => {
+            var user = Users[assignee]
             if (user) {
-              console.log('NOTIFY: ' + assignee);
-              Bot.startPrivateConversation({ user: user }, function(err,convo) {
-                convo.say('New PR!');
-              });
+              console.log("NOTIFY: " + assignee)
+              Bot.startPrivateConversation({ user: user }, (err, convo) => {
+                if (err) {
+                  console.error(err)
+                } else {
+                  convo.say("New PR!")
+                }
+              })
             }
-          });
+          })
         }
       }
     }
-    res.status(201).end();
-  });
-});
+    res.status(201).end()
+  })
+})
